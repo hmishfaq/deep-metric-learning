a4bcdf060d8f20119173d9595b67682c2f919d18
hmishfaq <hmishfaq@stanford.edu>
2017-06-01 19:35:44 -0700

    autoencoder

--- triplet-network-autoencoder/decoder.py
+++ triplet-network-autoencoder/decoder.py
@@ -1,3 +1,13 @@
+from __future__ import print_function
+import random 
+import shutil
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init 
+from torch.autograd import Variable
+import numpy as np
+
 
 class Decoder(nn.Module):
     '''
--- triplet-network-autoencoder/vgg.py
+++ triplet-network-autoencoder/vgg.py
@@ -7,3 +7,38 @@ import torch.nn.functional as F
 import torch.nn.init as init 
 from torch.autograd import Variable
 import numpy as np
+
+
+
+
+class VGG(nn.Module):
+    '''
+    Classic pre-trained VGG19 model.
+    Its forward call returns a list of the activations from
+    the predefined content layers.
+    '''
+
+    def __init__(self, ngpu):
+        super(_VGG, self).__init__()
+
+        self.ngpu = ngpu
+        features = models.vgg19(pretrained=True).features
+
+        self.features = nn.Sequential()
+        for i, module in enumerate(features):
+            name = layer_names[i]
+            self.features.add_module(name, module)
+
+    def forward(self, input):
+        batch_size = input.size(0)
+        all_outputs = []
+        output = input
+        for name, module in self.features.named_children():
+            if isinstance(output.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+                output = nn.parallel.data_parallel(
+                    module, output, range(self.ngpu))
+            else:
+                output = module(output)
+            if name in content_layers:
+                all_outputs.append(output.view(batch_size, -1))
+        return all_outputs
